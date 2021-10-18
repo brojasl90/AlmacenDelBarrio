5d21a51493ce532c60b916041e10445bffb33bc0
MGueL <mi.migue437@gmail.com>
2021-10-18 03:16:46 -0600

    UD 316

---
+++ appProyectoADB/ApplicationCore/Services/ServiceProducto.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceProducto : IServiceProducto
+    {
+        public Producto GetProductoByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Producto> GetProductos()
+        {
+            RepositoryProducto repository = new RepositoryProducto();
+            return repository.GetProductos();
+        }
+    }
+}
