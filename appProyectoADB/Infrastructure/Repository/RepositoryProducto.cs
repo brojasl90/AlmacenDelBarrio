2eaf0123d27274fecabd5db727f7b9b02ca3e2e3
MGueL <mi.migue437@gmail.com>
2021-10-18 02:56:38 -0600

    Update 3am

--- appProyectoADB/Infrastructure/Repository/RepositoryProducto.cs
+++ appProyectoADB/Infrastructure/Repository/RepositoryProducto.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Infrastructure.Models;
+using Infrastructure.Utils;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
 {
-    class RepositoryProducto
+    public class RepositoryProducto : IRepositoryProducto
     {
+        public Producto GetProductoByID(int id)
+        {
+            Producto oProducto = null;
+
+            using (MainContext ctx = new MainContext())
+            {
+                ctx.Configuration.LazyLoadingEnabled = false;
+                oProducto = ctx.Producto.
+                Where(l => l.IdProducto == id).
+                Include(a => a.Nombre_Producto).
+                Include(d => d.Descripcion).
+                 Include(p => p.Precio).
+                  Include(c => c.CantidadDisponible).
+                FirstOrDefault();
+            }
+            return oProducto;
+        }
+
+        public IEnumerable<Producto> GetProductos()
+        {
+            
+            try
+            {
+                IEnumerable<Producto> lista = null;
+                using (MainContext ctx = new MainContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    lista = ctx.Producto.ToList<Producto>();
+                }
+                return lista;
+            }
+
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
     }
 }
