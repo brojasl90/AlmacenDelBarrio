d1e77af0a20c11c8f339f9746fc886dfd8253bbb
MGueL <mi.migue437@gmail.com>
2021-10-18 03:07:49 -0600

    ProblemaOK

--- appProyectoADB/Web/Utils/Log.cs
+++ appProyectoADB/Web/Utils/Log.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Web;
 
 namespace Web.Utils
